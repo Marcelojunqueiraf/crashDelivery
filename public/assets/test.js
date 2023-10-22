682ea32fd0d322e642537a085c98a65ea8f0b8b4
Edson Sales <edsondesouzasales@gmail.com>
2023-10-22 09:56:32 -0300

    fix:Ajusta hitbox dependendo do frame

--- public/assets/test.js
+++ public/assets/test.js
@@ -1,27 +1,34 @@
-var loop;
-var playingGroup;
-function preload() {
-  game.load.spritesheet("rpsLeft", "assets/rps-left.png", 92, 86);
-}
-function dynamicSprite(x, key) {
-  loop = game.add.sprite(x, game.world.centerY - 35, key);
-  loop.animations.add("play", [0, 1, 2]);
-  loop.animations.play("play", 20, true);
-  loop.smoothed = false;
-  playingGroup.create(loop);
-}
-btn = game.add.button(
-  game.world.centerX - 70,
-  game.world.centerY + 105,
-  "rockSprite",
-  this.btnRock,
-  this,
-  1,
-  0,
-  2,
-  0
-);
-btn.input.useHandCursor = true;
-function btnRock() {
-  // here
+function create() {
+  // make the player
+  player = game.add.sprite(0, 0, "mario");
+  // create a group for all the player's hitboxes
+  hitboxes = game.add.group();
+  // give all the hitboxes a physics body (I'm using arcade physics btw)
+  hitboxes.enableBody = true;
+  // make the hitboxes children of the player. They will now move with the player
+  player.addChild(hitboxes);
+  // create a "hitbox" (really just an empty sprite with a physics body)
+  var hitbox1 = hitboxes.create(0, 0, null);
+  // set the size of the hitbox, and its position relative to the player
+  hitbox1.body.setSize(50, 50, player.width, player.height / 2);
+   // add some properties to the hitbox. These can be accessed later for use in calculations
+  hitbox1.name = "punch";
+  hitbox1.damage = 50;
+  hitbox1.knockbackDirection = 0.5;
+  hitbox1.knockbackAmt = 600;
+} // activate a hitbox by name
+function enableHitbox(hitboxName) {
+  // search all the hitboxes
+  for (var i = 0; i < hitboxes.children.length; i++) {
+    // if we find the hitbox with the "name" specified
+    if (hitboxes.children[i].name === hitboxName) {
+      // reset it
+      hitboxes.children[i].reset(0, 0);
+    }
+  }
+} // disable all active hitboxes
+function disableAllHitboxes() {
+  hitboxes.forEachExists(function (hitbox) {
+    hitbox.kill();
+  });
 }
--- src/scenes/Car.js
+++ src/scenes/Car.js
@@ -2,11 +2,9 @@ import Phaser from "phaser";
 import { toRadians } from "../utils/toRadian";
 
 export default class Car extends Phaser.Scene {
-
-  carAcceleration = new Phaser.Math.Vector2(5, 0)
-  friction = 0.01
-  currentFrame = 2
-  
+  carAcceleration = new Phaser.Math.Vector2(5, 0);
+  friction = 0.01;
+  counter = 0;
   preload() {
     this.load.image("tiles", "/assets/tilemap.png");
     this.load.tilemapTiledJSON("map", "/assets/map.json");
@@ -17,42 +15,54 @@ export default class Car extends Phaser.Scene {
   }
 
   create() {
-
-    const map = this.make.tilemap({ key: "map", tileWidth: 32, tileHeight: 32 });
+    const map = this.make.tilemap({
+      key: "map",
+      tileWidth: 32,
+      tileHeight: 32,
+    });
     // Parameters are the name you gave the Tiled Editor and then the key of the tileset image in loadImage
     const tileset = map.addTilesetImage("tiles", "tiles");
     // Parameters: layer name from Tiled Editor, tileset, x, y
-    const streetsLayer = map.createLayer("streets", tileset, 0, 0)
-    const buildingsLayer = map.createLayer("buildings", tileset, 0, 0)
-    const detailsLayer = map.createLayer("details", tileset, 0, 0)
+    const streetsLayer = map.createLayer("streets", tileset, 0, 0);
+    const buildingsLayer = map.createLayer("buildings", tileset, 0, 0);
+    const detailsLayer = map.createLayer("details", tileset, 0, 0);
 
-    this.car = this.physics.add.sprite(30, 15, "car")
+    this.car = this.physics.add.sprite(30, 15, "car");
     this.car.body.setBounce(1, 1);
-    
-    buildingsLayer.setCollisionByExclusion([-1])
-    this.physics.add.collider(this.car, buildingsLayer)
+
+    buildingsLayer.setCollisionByExclusion([-1]);
+    this.physics.add.collider(this.car, buildingsLayer);
 
     this.cameras.main.startFollow(this.car);
 
-    const totalFrames = 16
-    const frames = [...Array(totalFrames).keys()]
+    const totalFrames = 16;
+    const frames = [...Array(totalFrames).keys()];
 
     this.anims.create({
-      key: 'right',
-      frames: this.anims.generateFrameNumbers('car', {start: 0, end: totalFrames - 1}),
+      key: "right",
+      frames: this.anims.generateFrameNumbers("car", {
+        start: 0,
+        end: totalFrames - 1,
+      }),
       frameRate: 10,
-      repeat: -1
-    })
+      repeat: -1,
+    });
 
     this.anims.create({
-      key: 'left',
-      frames: this.anims.generateFrameNumbers('car', {start: totalFrames-1, end: 0}),
+      key: "left",
+      frames: this.anims.generateFrameNumbers("car", {
+        start: totalFrames - 1,
+        end: 0,
+      }),
       frameRate: 10,
-      repeat: -1
-    })
+      repeat: -1,
+    });
 
-    this.car.anims.play('left', true)
-    this.car.anims.play('right', true)
+    this.car.setSize(50, 32);
+
+    this.car.anims.play("left", true);
+    this.car.anims.play("right", true);
+    this.car.setFrame(10);
   }
   update() {
     const up = this.input.keyboard.addKey("up");
@@ -60,35 +70,37 @@ export default class Car extends Phaser.Scene {
     const left = this.input.keyboard.addKey("left");
     const right = this.input.keyboard.addKey("right");
 
+    // Adjust hitbox
+    if ([6, 7, 8, 13, 14, 15].includes(this.car.anims.currentFrame?.index))
+      this.car.setSize(32, 32);
+    else this.car.setSize(50, 32);
 
     if (up.isDown) {
-      this.carAcceleration.setAngle(toRadians(this.car.body.rotation))
-      this.car.body.velocity.add(this.carAcceleration)
+      this.carAcceleration.setAngle(toRadians(this.car.body.rotation));
+      this.car.body.velocity.add(this.carAcceleration);
     } else if (down.isDown) {
-      this.carAcceleration.setAngle(toRadians(this.car.body.rotation))
-      this.car.body.velocity.subtract(this.carAcceleration)
-    } 
+      this.carAcceleration.setAngle(toRadians(this.car.body.rotation));
+      this.car.body.velocity.subtract(this.carAcceleration);
+    }
 
     if (left.isDown) {
       //this.car.body.rotation -= 5;
-      const currentFrame = this.car.anims.currentFrame
-      this.car.anims.playReverse('left', true)
-      this.car.anims.setCurrentFrame(currentFrame)
-      
+      const currentFrame = this.car.anims.currentFrame;
+      this.car.anims.playReverse("left", true);
+      this.car.anims.setCurrentFrame(currentFrame);
     } else if (right.isDown) {
       //this.car.body.rotation += 5;
-      const currentFrame = this.car.anims.currentFrame
-      this.car.anims.play('right', true)
-      this.car.anims.setCurrentFrame(currentFrame)
-    }
-    else {
-      //this.currentFrame = this.car.anims.currentFrame
+      const currentFrame = this.car.anims.currentFrame;
+      this.car.anims.play("right", true);
+      this.car.anims.setCurrentFrame(currentFrame);
+    } else {
       this.car.anims.stop();
     }
 
     // Friction
-    if (this.car.body.velocity.length() > 0) 
-      this.car.body.velocity.subtract(this.car.body.velocity.clone().scale(this.friction))
+    if (this.car.body.velocity.length() > 0)
+      this.car.body.velocity.subtract(
+        this.car.body.velocity.clone().scale(this.friction)
+      );
   }
-
 }
