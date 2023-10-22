3692e46df04e81698a67cd8724d1a9501f794a70
Edson Sales <edsondesouzasales@gmail.com>
2023-10-22 00:12:56 -0300

    feat:adiciona imagem para carro

--- src/scenes/Car.js
+++ src/scenes/Car.js
@@ -7,33 +7,27 @@ export default class Car extends Phaser.Scene {
   friction = 0.01
   
   preload() {
-    this.load.image("tiles", "/assets/tilemap-extruded.png");
+    this.load.image("tiles", "/assets/tilemap.png");
     this.load.tilemapTiledJSON("map", "/assets/map.json");
+    this.load.image("car", "/assets/car.png");
   }
 
   create() {
 
-    const map = this.make.tilemap({ key: "map", tileWidth: 16, tileHeight: 16 });
-    map.setLayerTileSize(16, 16);
+    const map = this.make.tilemap({ key: "map", tileWidth: 32, tileHeight: 32 });
     // Parameters are the name you gave the Tiled Editor and then the key of the tileset image in loadImage
-    const tileset = map.addTilesetImage("tiles1", "tiles");
+    const tileset = map.addTilesetImage("tiles", "tiles");
     // Parameters: layer name from Tiled Editor, tileset, x, y
-    const layer = map.createLayer("street", tileset, 0, 0)
-    
-    map.layers.forEach(layer => {
-      layer.tilemapLayer.scale = 2
-    })
+    const streetsLayer = map.createLayer("streets", tileset, 0, 0)
+    const buildingsLayer = map.createLayer("buildings", tileset, 0, 0)
+    const detailsLayer = map.createLayer("details", tileset, 0, 0)
 
-    this.car = this.add.rectangle(400, 300, 30, 15, 0xff5555);
+    this.car = this.add.image(30, 15, "car")
     this.physics.add.existing(this.car);
-    //this.car.body.setCollideWorldBounds(true);
     this.car.body.setBounce(1, 1);
-
-    this.wall = this.add.rectangle(50, 400, 20, 100, 0xffffff);
-    this.physics.add.existing(this.wall);
-    this.wall.body.setImmovable(true);
-
-    this.physics.add.collider(this.car, this.wall);
+    
+    buildingsLayer.setCollisionByExclusion([-1])
+    this.physics.add.collider(this.car, buildingsLayer)
 
     this.cameras.main.startFollow(this.car);
 
